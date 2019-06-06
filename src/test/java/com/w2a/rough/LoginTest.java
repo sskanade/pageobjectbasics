ec2d2360d6c812a4216241962a4b811e7557cf9e
Shlok <smkanade@yahoo.com>
2019-06-06 12:31:43 +1000

    The first commit

---
+++ src/test/java/com/w2a/rough/LoginTest.java
@@ -0,0 +1,29 @@
+package com.w2a.rough;
+
+import com.w2a.base.Page;
+import com.w2a.pages.HomePage;
+import com.w2a.pages.LoginPage;
+import com.w2a.pages.ZohoAppPage;
+import com.w2a.pages.crm.accounts.AccountsPage;
+import com.w2a.pages.crm.accounts.CreateAccountPage;
+
+public class LoginTest {
+
+	public static void main(String[] args) {
+
+
+		HomePage home = new HomePage();
+		LoginPage lp = home.goToLogin();
+		ZohoAppPage zp = lp.doLogin("smkanade@yahoo.com", "Manali@2011");
+		zp.gotoCRM();
+		AccountsPage account = Page.menu.gotoAccounts();
+		CreateAccountPage cap = account.gotoCreateAccounts();
+		cap.createAccount("Santosh");
+		
+		//driver.manage.window.maximze()
+
+		
+		
+	}
+
+}
